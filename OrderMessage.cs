a94ee8ba8ce808498db321d1c87416fbffee8ec6
= <craig@craigshoemaker.net>
2020-02-28 14:48:07 -0500

    Add DTO class

---
+++ OrderMessage.cs
@@ -0,0 +1,14 @@
+
+namespace Demo {
+
+  public class OrderMessage
+  {
+    public string orderId { get; set; }
+    public string name { get; set; }
+    public string email { get; set; }
+    public string productId { get; set; }
+    public string quantity { get; set; }
+    public string key { get; set; }
+  }
+  
+}
